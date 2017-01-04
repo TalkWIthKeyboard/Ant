09cdbfe6af347c9426e03219fcec92b0824f7794
宋伟 <610347922@qq.com>
2017-01-04 09:03:54 +0800

    1. task页面的数据修改

--- src/client/views/task.vue
+++ src/client/views/task.vue
@@ -2,8 +2,8 @@
   <div id="task-main">
     <img :src="qrCode">
     <div id="cell-list">
-      <mt-cell title="起点" :value="item.parcel.senderAddress"></mt-cell>
-      <mt-cell title="终点" :value="item.parcel.receiverAddress"></mt-cell>
+      <mt-cell title="起点" :value="item.parcelPath.start"></mt-cell>
+      <mt-cell title="终点" :value="item.parcelPath.end"></mt-cell>
       <mt-cell title="物品" :value="item.parcel.content"></mt-cell>
       <mt-cell title="重量" :value="item.parcel.str"></mt-cell>
       <mt-cell title="创建时间" value="item.createdAt"></mt-cell>
@@ -12,56 +12,56 @@
 </template>
 
 <script>
-export default{
-  data(){
-    return{
-      qrCode: "http://oj7mt8loy.bkt.clouddn.com/qrCode.jpg",
-      item: {
-        parcel: {
-          senderAddress: null,
-          receiverAddress: null,
-          content: null,
-          str: null
-        },
-        createdAt: null
-      }
-    }
-  },
-
-  components:{
-
-  },
-
-  methods: {
-    // 调用api获取数据
-    getData: function () {
-      var that = this;
-      var url = 'https://ant-express.picfood.cn/api/parcel/path/confirm?id=' + this.$route.params.id;
-      $.ajax({
-          url: url,
-          type: 'get',
-          dataType: 'json',
-          xhrFields: {
-            withCredentials: true
+  export default{
+    data(){
+      return {
+        qrCode: "http://oj7mt8loy.bkt.clouddn.com/qrCode.jpg",
+        item: {
+          parcel: {
+            content: null,
+            str: null
           },
-          success: function (data) {
-            that.item = data.result;
-            that.item.parcel.str = that.item.parcel.weight + " " + that.item.parcel.unit;
+          parcelPath: {
+            start: null,
+            end: null
           },
-          error: function (data) {
-            console.log("error " + data);
-          }
+          createdAt: null
         }
-      )
-    }
-  },
+      }
+    },
 
-  created: function () {
-    this.$parent.isShowStepper = true;
-    this.$parent.$children[1].activeStep = 2;
-    this.getData();
+    components: {},
+
+    methods: {
+      // 调用api获取数据
+      getData: function () {
+        var that = this;
+        var url = 'https://ant-express.picfood.cn/api/parcel/path/confirm?id=' + this.$route.params.id;
+        $.ajax({
+            url: url,
+            type: 'get',
+            dataType: 'json',
+            xhrFields: {
+              withCredentials: true
+            },
+            success: function (data) {
+              that.item = data.result;
+              that.item.parcel.str = that.item.parcel.weight + " " + that.item.parcel.unit;
+            },
+            error: function (data) {
+              console.log("error " + data);
+            }
+          }
+        )
+      }
+    },
+
+    created: function () {
+      this.$parent.isShowStepper = true;
+      this.$parent.$children[1].activeStep = 2;
+      this.getData();
+    }
   }
-}
 </script>
 
 <style>
